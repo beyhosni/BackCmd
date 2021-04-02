897e37be6ede71ffe90448393513f682e0d81bc2
Hosni Bey <beyhosni@gmail.com>
2021-04-03 01:02:19 +0200

    [Cmd app] : controller update

--- src/controllers/cmdController.js
+++ src/controllers/cmdController.js
@@ -1,6 +1,6 @@
 const db = require("../models");
-const Tutorial = db.tutorials;
-// Create and Save a new Tutorial
+const Cmde = db.cmd;
+// Create and Save a new cmd
 exports.create = (req, res) => {
     // Validate request
     if (!req.body.title) {
@@ -8,15 +8,15 @@ exports.create = (req, res) => {
         return;
     }
 
-    // Create a Tutorial
-    const tutorial = new Tutorial({
+    // Create a cmd
+    const cmd = new Cmde({
         title: req.body.title,
         description: req.body.description,
         isReady: req.body.isReady ? req.body.isReady : false,
         location:req.body.location
     });
 
-    // Save Tutorial in the database
+    // Save cmd in the database
     cmd
         .save(cmd)
         .then(data => {
@@ -30,41 +30,41 @@ exports.create = (req, res) => {
         });
 };
 
-// Retrieve all Tutorials from the database.
+// Retrieve all Commandes from the database.
 exports.findAll = (req, res) => {
     const title = req.query.title;
     var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
-    Tutorial.find(condition)
+    Cmde.find(condition)
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                err.message || "Some error occurred while retrieving tutorials."
+                err.message || "Some error occurred while retrieving commandes."
             });
         });
 };
 
-// Find a single Tutorial with an id
+// Find a single Commande with an id
 exports.findOne = (req, res) => {
     const id = req.params.id;
 
-    Tutorial.findById(id)
+    Cmde.findById(id)
         .then(data => {
             if (!data)
-                res.status(404).send({ message: "Not found Tutorial with id " + id });
+                res.status(404).send({ message: "Not found Commande with id " + id });
             else res.send(data);
         })
         .catch(err => {
             res
                 .status(500)
-                .send({ message: "Error retrieving Tutorial with id=" + id });
+                .send({ message: "Error retrieving Commande with id=" + id });
         });
 };
 
-// Update a Tutorial by the id in the request
+// Update a Commande by the id in the request
 exports.update = (req, res) => {
     if (!req.body) {
         return res.status(400).send({
@@ -74,70 +74,70 @@ exports.update = (req, res) => {
 
     const id = req.params.id;
 
-    Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    Cmde.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+                    message: `Cannot update commande `
                 });
-            } else res.send({ message: "Tutorial was updated successfully." });
+            } else res.send({ message: "Commande was updated successfully." });
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating Tutorial with id=" + id
+                message: "Error updating commande with id=" + id
             });
         });
 };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Commande with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
 
-    Tutorial.findByIdAndRemove(id, { useFindAndModify: false })
+    Cmde.findByIdAndRemove(id, { useFindAndModify: false })
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                    message: `Cannot delete commande`
                 });
             } else {
                 res.send({
-                    message: "Tutorial was deleted successfully!"
+                    message: "Commande was deleted successfully!"
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Could not delete Tutorial with id=" + id
+                message: "Could not delete Commande with id=" + id
             });
         });
 };
 
-// Delete all Tutorials from the database.
+// Delete all Commande from the database.
 exports.deleteAll = (req, res) => {
-    Tutorial.deleteMany({})
+    Cmde.deleteMany({})
         .then(data => {
             res.send({
-                message: `${data.deletedCount} Tutorials were deleted successfully!`
+                message: `${data.deletedCount} commande were deleted successfully!`
             });
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                err.message || "Some error occurred while removing all tutorials."
+                err.message || "Some error occurred while removing all commandes."
             });
         });
 };
 
-// Find all published Tutorials
+// Find all published Commandes
 exports.findAllPublished = (req, res) => {
-    Tutorial.find({ published: true })
+    Cmde.find({ published: true })
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                err.message || "Some error occurred while retrieving tutorials."
+                err.message || "Some error occurred while retrieving Commandes."
             });
         });
 };
